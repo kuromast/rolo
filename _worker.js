b1474c8d9fb4e147954344e8da58602a98334dd8
kuromast <105215044+kuromast@users.noreply.github.com>
2022-06-27 05:20:37 +0100

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,23 +1,13 @@
 export default {
-
   async fetch(request, env) {
-
     let url = new URL(request.url);
-
     if (url.pathname.startsWith('/')) {
-
       url.hostname = 'yes.945984nh496.workers.dev'
-
       let new_request = new Request(url, request);
-
       return fetch(new_request);
-
     }
-
     return env.ASSETS.fetch(request);
-
   },
-
 };
 
 
